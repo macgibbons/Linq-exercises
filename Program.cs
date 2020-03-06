a547c2d5ecc026b6d34b0d657ac6e53d7b3463d3
macgibbons <macgibbons@gmail.com>
2020-03-06 15:29:34 -0600

    practice using .count, .sum, .max

--- Program.cs
+++ Program.cs
@@ -90,6 +90,57 @@ namespace linq
             };
 
             var ascend = numbers2.OrderBy (num => num);
+
+            // Output how many numbers are in this list
+            List<int> numbers3 = new List<int> ()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            var numberCount = numbers3.Count;
+
+            // How much money have we made?
+            List<double> purchases = new List<double> ()
+            {
+                2340.29,
+                745.31,
+                21.76,
+                34.03,
+                4786.45,
+                879.45,
+                9442.85,
+                2454.63,
+                45.65
+            };
+
+            var Sum = purchases.Sum ();
+            // What is our most expensive product?
+            List<double> prices = new List<double> ()
+            {
+                879.45,
+                9442.85,
+                2454.63,
+                45.65,
+                2340.29,
+                34.03,
+                4786.45,
+                745.31,
+                21.76
+            };
+            var MostExpensive = prices.Max ();
         }
     }
 }
